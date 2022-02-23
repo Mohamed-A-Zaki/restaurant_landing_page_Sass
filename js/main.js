6102210d1a8fa476e7be861cbb423153e481c195
Mohamed-A-Zaki <mohamedzaki385@gmail.com>
2022-02-23 18:33:09 +0200

    add scroll to top button

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ let nav_content = document.querySelector("nav .content");
 nav_content.onclick = function () {
   links.classList.toggle("show");
 };
+
+/**
+ * scroll-to-top
+ */
+let scroll_to_top = document.querySelector(".scroll-to-top");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 1000
+    ? scroll_to_top.classList.add("show")
+    : scroll_to_top.classList.remove("show");
+});
+
+scroll_to_top.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
